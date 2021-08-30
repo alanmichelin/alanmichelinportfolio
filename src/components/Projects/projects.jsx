bbefb3acdba116dc6287d43a3c886e03ed057db4
alanmichelin <alan17pc@gmail.com>
2021-08-30 20:48:15 -0300

    fixed projects text

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -15,12 +15,13 @@ import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
         <Grid item xs={12} sm={12}>
             <Paper className={classes.paper} ><p style={{fontSize:'24px'}}><strong>Projects:</strong></p></Paper>
             </Grid>
-            <Project name={`PongJS`} description={`PongJS is just the PONG videogame made with pure JavaScript.<br/>
+            <Project name={`PongJS`} description={`PongJS is just the PONG videogame made with pure JavaScript.
+            
                 My objective was to apply all my html and css knowledge adding some logic with JavaScript while i experimented with flexbox, positioning, zIndex, etc. `} img={PongJS} tech={`HTML - CSS - JavaScript`} linkDemo={'https://alanmichelin.github.io/pongjs/'} linkSrc={'https://github.com/alanmichelin/pongjs'}/>
-            <Project name={`VideoDownloader`} description={`As title says, this is an app that can download videos from Facebook, Instagram and Youtube. <br/>
-                I've learned a lot while doing this app, since everything must be asynchronic. <br/>The way it works with Facebook and Instagram, is by intercepting the media response of a video, getting the mp4 and creating a file.
+            <Project name={`VideoDownloader`} description={`As title says, this is an app that can download videos from Facebook, Instagram and Youtube. 
+                I've learned a lot while doing this app, since everything must be asynchronic. The way it works with Facebook and Instagram, is by intercepting the media response of a video, getting the mp4 and creating a file.
                 With youtube it was a lot easier because i used a library`} img={VideoDownloader} tech={`JavaScript, NodeJS, Puppeteer, YTDL-Core`} linkDemo={'https://github.com/alanmichelin/VideoDownloader/releases/tag/1.0.1beta'} linkSrc={'https://github.com/alanmichelin/VideoDownloader'}/>
-            <Project name={`FrontEnd e-commerce`} description={`This is the last project i did. With this one i've learned a lot about React foundaments(split code in components, useState, useEffect, optimizing and making reusable code) along with some JavaScript logic. <br/>
+            <Project name={`FrontEnd e-commerce`} description={`This is the last project i did. With this one i've learned a lot about React foundaments(split code in components, useState, useEffect, optimizing and making reusable code) along with some JavaScript logic. 
                 The way it works is by fetching a random food api, you choose any category, it fetchs the api again, and display new results.`} img={Ecommerce} tech={`JavaScript, React, Redux, React-Bootstrap`} linkDemo={'https://randomapiecommerce.netlify.app/'} linkSrc={'https://github.com/alanmichelin/frontend-ecommerce'}/>
     </Grid>
     )}
